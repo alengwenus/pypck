35a31629555465fd66d07b6f303780cf3f5df8da
Andre Lengwenus <alengwenus@gmail.com>
2018-10-27 13:51:49 +0200

    updated readme and generated setup.py

---
+++ README.md
@@ -0,0 +1,10 @@
+# pypck
+Open source LCN-PCK library written in Python
+
+### Overview
+
+**pypck** is an open source library written in Python which allows the connection to the [LCN (local control network) system](https://www.lcn.eu). It uses the vendor protocol LCN-PCK.
+To get started a free license of the coupling software LCN-PCHK and a hardware coupler is necessary.
+The library is a prerequisite for the [Home Assistant](https://www.home-assistant.io/) LCN component and is based entirely on Python's asyncio.
+
+**pypck** is a derivative of the [LCN openHAB binding](https://github.com/openhab/openhab1-addons/wiki/lcn-binding) from Tobias Juettner with several inprovements.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pypck",
+    version="0.9.0",
+    author="Andre Lengwenus",
+    author_email="alengwenus@gmail.com",
+    description="LCN-PCK library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alengwenus/pypck",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Eclipse Public License 1.0 (EPL-1.0)",
+        "Operating System :: OS Independent",
+        "Topic :: Home Automation"
+    ],
+)
