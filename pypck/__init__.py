2457643d1bb521f1f839c6622a763b4d55595bde
Andre Lengwenus <alengwenus@gmail.com>
2018-12-20 14:48:05 +0100

    Added __all__ in __init__.py for public modules

--- pypck/__init__.py
+++ pypck/__init__.py
@@ -1,3 +1,6 @@
 """PYPCK module"""
 from pypck import (connection, inputs, lcn_addr, lcn_defs, module,
                    pck_commands, timeout_retry)
+
+__all__ = [connection, inputs, lcn_addr, lcn_defs, module, pck_commands,
+           timeout_retry]
