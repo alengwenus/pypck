3bc5c49b6469650eef1a049e4595ca8169f8c4fc
Andre Lengwenus <alengwenus@gmail.com>
2020-11-06 14:15:24 +0000

    Add a received method to pypck_client fixture to wait for messages sent from pchk server.

--- tests/test_segment_couplers.py
+++ tests/test_segment_couplers.py
@@ -1,5 +1,7 @@
 """Segment coupler tests"""
 
+import asyncio
+
 import pytest
 
 
@@ -12,3 +14,16 @@ async def test_segment_coupler_search(pchk_server, pypck_client):
     assert await pchk_server.received(b">G003003.SK")
     assert await pchk_server.received(b">G003003.SK")
     assert await pchk_server.received(b">G003003.SK")
+
+
+@pytest.mark.asyncio
+async def test_segment_coupler_response(pchk_server, pypck_client):
+    """Test segment coupler response."""
+    await pypck_client.async_connect()
+
+    assert pypck_client.local_seg_id == 0
+
+    await pchk_server.send_message("=M000005.SK020")
+    assert await pypck_client.received("=M000005.SK020")
+
+    assert pypck_client.local_seg_id == 20
