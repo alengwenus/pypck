634c2f6adfd4881bce098e036cba7901db68b813
maximilianriemensberger <maximilianriemensberger@users.noreply.github.com>
2020-11-17 22:27:25 +0100

    Refresh the docs and add to tox (#38)
    * Remove explicit members from connection api docs recipe
    
    * Fix docstring in module.py
    
    * Add a docs build state to tox.ini
    
    * Switch from classic to nature theme
    
    Co-authored-by: Maximilian Riemensberger <riemensberger@cadami.net>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,12 +70,12 @@ exclude_patterns = []
 pygments_style = 'default'
 
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -86,17 +86,19 @@ html_theme = 'classic'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
-# The default sidebars (for documents that don't match any pattern) are
-# defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-#
-# html_sidebars = {}
+# This is required for the alabaster theme
+# refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
+html_sidebars = {
+    '**': [
+        'relations.html',  # needs 'show_related': True theme option to display
+        'searchbox.html',
+    ]
+}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
