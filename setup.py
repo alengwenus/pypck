32ba85c6967ead25cbc5200e4fe4c73477a2b026
Andre Lengwenus <alengwenus@gmail.com>
2020-12-01 19:24:55 +0000

    Bump to 0.7.7

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="pypck",
-    version="0.7.6",
+    version="0.7.7",
     author="Andre Lengwenus",
     author_email="alengwenus@gmail.com",
     description="LCN-PCK library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alengwenus/pypck",
-    packages=setuptools.find_packages(exclude=(["tests","tests.*"])),
+    packages=setuptools.find_packages(exclude=(["tests", "tests.*"])),
     install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
