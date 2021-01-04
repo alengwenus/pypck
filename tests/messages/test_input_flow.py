6a7539cb3571240ad67f54c9bdce1323ef0c5f39
Andre Lengwenus <alengwenus@gmail.com>
2021-01-04 22:03:47 +0000

    Handle segment id 4 the same way as segment id 0.

--- tests/messages/test_input_flow.py
+++ tests/messages/test_input_flow.py
@@ -2,7 +2,8 @@
 from unittest.mock import AsyncMock, patch
 
 import pytest
-from pypck.inputs import Input
+from pypck.inputs import Input, ModInput
+from pypck.lcn_addr import LcnAddr
 
 
 @pytest.mark.asyncio
@@ -16,3 +17,23 @@ async def test_message_to_input(pypck_client):
 
     inp_parse.assert_called_with(message)
     pypck_client.async_process_input.assert_awaited_with(inp)
+
+
+@pytest.mark.asyncio
+async def test_physical_to_logical_segment_id(pypck_client):
+    """Test conversion from logical to physical segment id."""
+    pypck_client.local_seg_id = 20
+    module = pypck_client.get_address_conn(LcnAddr(20, 7, False))
+    module.async_process_input = AsyncMock()
+
+    with patch("tests.conftest.MockPchkConnectionManager.is_ready", result=True):
+        inp = ModInput(LcnAddr(20, 7, False))
+        await pypck_client.async_process_input(inp)
+
+        inp = ModInput(LcnAddr(0, 7, False))
+        await pypck_client.async_process_input(inp)
+
+        inp = ModInput(LcnAddr(4, 7, False))
+        await pypck_client.async_process_input(inp)
+
+        assert module.async_process_input.await_count == 3
