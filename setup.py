8e67c01838d4a02d96f0be5bd352ceb324a60fdb
Andre Lengwenus <alengwenus@gmail.com>
2021-07-08 05:43:11 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="pypck",
-    version="0.7.9",
+    version="0.7.10",
     author="Andre Lengwenus",
     author_email="alengwenus@gmail.com",
     description="LCN-PCK library",
