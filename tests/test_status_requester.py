7bc35e1ee8356dfa7af3186dcef0cb42452b2cb1
Andre Lengwenus <alengwenus@gmail.com>
2025-12-17 06:59:27 +0000

    Remove tests for stored status request

--- tests/test_status_requester.py
+++ tests/test_status_requester.py
@@ -1,7 +1,6 @@
 """Test the status requester of a module connection."""
 
 import asyncio
-from unittest.mock import call
 
 from pypck.pck_commands import PckGenerator
 from pypck.status_requester import StatusRequest
@@ -39,32 +38,32 @@ async def test_request_status(module10: MockDeviceConnection) -> None:
     assert result.states == RELAY_STATES
 
 
-async def test_request_status_stored(module10: MockDeviceConnection) -> None:
-    """Test requesting the status of a module with stored status request."""
-    status_request = StatusRequest(
-        type=inputs.ModStatusRelays,
-        parameters=frozenset(),
-        timestamp=asyncio.get_running_loop().time(),
-        response=asyncio.get_running_loop().create_future(),
-    )
-    status_request.response.set_result(
-        inputs.ModStatusRelays(module10.addr, RELAY_STATES)
-    )
-    module10.status_requester.last_requests.add(status_request)
+# async def test_request_status_stored(module10: MockDeviceConnection) -> None:
+#     """Test requesting the status of a module with stored status request."""
+#     status_request = StatusRequest(
+#         type=inputs.ModStatusRelays,
+#         parameters=frozenset(),
+#         timestamp=asyncio.get_running_loop().time(),
+#         response=asyncio.get_running_loop().create_future(),
+#     )
+#     status_request.response.set_result(
+#         inputs.ModStatusRelays(module10.addr, RELAY_STATES)
+#     )
+#     module10.status_requester.last_requests.add(status_request)
 
-    result = await module10.status_requester.request(
-        response_type=inputs.ModStatusRelays,
-        request_pck=PckGenerator.request_relays_status(),
-        max_age=10,
-    )
+#     result = await module10.status_requester.request(
+#         response_type=inputs.ModStatusRelays,
+#         request_pck=PckGenerator.request_relays_status(),
+#         max_age=10,
+#     )
 
-    assert isinstance(result, inputs.ModStatusRelays)
-    assert result.physical_source_addr == module10.addr
-    assert result.states == RELAY_STATES
-    assert (
-        call(False, PckGenerator.request_relays_status())
-        not in module10.send_command.await_args_list
-    )
+#     assert isinstance(result, inputs.ModStatusRelays)
+#     assert result.physical_source_addr == module10.addr
+#     assert result.states == RELAY_STATES
+#     assert (
+#         call(False, PckGenerator.request_relays_status())
+#         not in module10.send_command.await_args_list
+#     )
 
 
 async def test_request_status_expired(module10: MockDeviceConnection) -> None:
