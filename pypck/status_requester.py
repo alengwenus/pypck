762843f454b7ccf37c44fcefb219fec88b21f310
Andre Lengwenus <alengwenus@gmail.com>
2025-12-27 15:09:35 +0100

    Add request cache (#177)
    * Add request cache
    
    * Add tests
    
    * Improve logging command

--- pypck/status_requester.py
+++ pypck/status_requester.py
@@ -28,13 +28,13 @@ class StatusRequest(Generic[ResponseT]):
     )  # timestamp the response was received; -1=no timestamp
     response: asyncio.Future[ResponseT] = field(
         compare=False
-    )  # Future to hold the response input object
+    )  # the response input object
 
 
 class StatusRequester:
     """Handling of status requests."""
 
-    current_request: StatusRequest[inputs.Input]
+    current_request: StatusRequest[inputs.Input] | None
 
     def __init__(
         self,
@@ -42,24 +42,61 @@ class StatusRequester:
     ) -> None:
         """Initialize the context."""
         self.device_connection = device_connection
-        self.last_requests: set[StatusRequest[inputs.Input]] = set()
+        self.current_request = None
+        self.request_cache: set[StatusRequest[inputs.Input]] = set()
         self.max_response_age = self.device_connection.conn.settings["MAX_RESPONSE_AGE"]
         self.request_lock = asyncio.Lock()
 
+        self.unregister_inputs = self.device_connection.register_for_inputs(
+            self.input_callback
+        )
+
+    def get_status_requests(
+        self,
+        request_type: type[ResponseT],
+        parameters: frozenset[tuple[str, Any]] | None = None,
+        max_age: int = 0,
+    ) -> list[StatusRequest[ResponseT]]:
+        """Get the status requests for the given type and parameters."""
+        if parameters is None:
+            parameters = frozenset()
+        results = [
+            request
+            for request in self.request_cache
+            if request.type == request_type
+            and parameters.issubset(request.parameters)
+            and (
+                (request.timestamp == -1)
+                or (max_age == -1)
+                or (asyncio.get_running_loop().time() - request.timestamp < max_age)
+            )
+        ]
+        results.sort(key=lambda request: request.timestamp, reverse=True)
+        return cast(list[StatusRequest[ResponseT]], results)
+
     def input_callback(self, inp: inputs.Input) -> None:
         """Handle incoming inputs and set the result for the corresponding requests."""
+        # Update current request (if it exists)
         if (
-            self.current_request.response.done()
-            or self.current_request.response.cancelled()
+            self.current_request is not None
+            and not self.current_request.response.done()
         ):
-            return
+            if isinstance(inp, self.current_request.type) and all(
+                getattr(inp, parameter_name) == parameter_value
+                for parameter_name, parameter_value in self.current_request.parameters
+            ):
+                self.current_request.timestamp = asyncio.get_running_loop().time()
+                self.current_request.response.set_result(inp)
 
-        if isinstance(inp, self.current_request.type) and all(
-            getattr(inp, parameter_name) == parameter_value
-            for parameter_name, parameter_value in self.current_request.parameters
-        ):
-            self.current_request.timestamp = asyncio.get_running_loop().time()
-            self.current_request.response.set_result(inp)
+        # Update cached requests
+        for request in self.get_status_requests(type(inp)):
+            if all(
+                getattr(inp, parameter_name) == parameter_value
+                for parameter_name, parameter_value in request.parameters
+            ):
+                request.timestamp = asyncio.get_running_loop().time()
+                request.response = asyncio.get_running_loop().create_future()
+                request.response.set_result(inp)
 
     async def request(
         self,
@@ -71,15 +108,25 @@ class StatusRequester:
     ) -> ResponseT | None:
         """Execute a status request and wait for the response."""
         async with self.request_lock:
+            # check for matching request in cache
+            if requests := self.get_status_requests(
+                response_type,
+                frozenset(request_kwargs.items()),
+                max_age,
+            ):
+                _LOGGER.debug(
+                    "from %s: %s (cached)",
+                    self.device_connection.conn.connection_id,
+                    requests[0].response.result().pck,
+                )
+                return requests[0].response.result()
+
+            # no matching request in cache
             self.current_request = StatusRequest(
                 response_type,
                 frozenset(request_kwargs.items()),
                 -1,
                 asyncio.get_running_loop().create_future(),
-            )
-
-            unregister_inputs = self.device_connection.register_for_inputs(
-                self.input_callback
             )
 
             result = None
@@ -101,9 +148,8 @@ class StatusRequester:
                 except asyncio.CancelledError:
                     break
 
-            # if we got no results, remove the request from the set
-            if result is None:
-                self.current_request.response.cancel()
+            if result is not None:  # add request to cache
+                self.request_cache.discard(self.current_request)
+                self.request_cache.add(self.current_request)
 
-            unregister_inputs()
             return cast(ResponseT | None, result)
